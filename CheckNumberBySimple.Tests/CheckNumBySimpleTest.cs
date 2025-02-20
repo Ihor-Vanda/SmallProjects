bff30fbf5c03389a164af3b86a5c088889bd0ea3
Ihor-Vanda <vandaihor@gmail.com>
2025-02-20 13:59:57 +0200

    Optimized brute force algorithm for CheckNumBySimple

--- CheckNumberBySimple.Tests/CheckNumBySimpleTest.cs
+++ CheckNumberBySimple.Tests/CheckNumBySimpleTest.cs
@@ -5,10 +5,26 @@ namespace CheckNumberBySimple.Tests;
 public class CheckNumBySimpleTest
 {
     [Fact]
-    public void CheckNumBySimple_SimpleNumResult()
+    public void CheckNumBySimple_SimpleNumResultTrueWithMinimalsSimpleNumbers()
     {
-        Assert.True(CheckNumbySimple.IsSimpleNum(3));
-        Assert.True(CheckNumbySimple.IsSimpleNum(13));
-        Assert.False(CheckNumbySimple.IsSimpleNum(12));
+        Assert.True(CheckNumBySimple.IsSimpleNum(3));
+        Assert.True(CheckNumBySimple.IsSimpleNum(13));
+        Assert.False(CheckNumBySimple.IsSimpleNum(12));
+        Assert.True(CheckNumBySimple.IsSimpleNum(347));
+        Assert.True(CheckNumBySimple.IsSimpleNum(571));
+    }
+
+    [Fact]
+    public void CheckNumBySimple_SimpleNumResultFalse()
+    {
+        Assert.False(CheckNumBySimple.IsSimpleNum(12));
+        Assert.False(CheckNumBySimple.IsSimpleNum(200));
+    }
+
+    [Fact]
+    public void CheckNumBySimple_SimpleNumResultTrueWithHightValueSimpleNumbers()
+    {
+        Assert.True(CheckNumBySimple.IsSimpleNum(347));
+        Assert.True(CheckNumBySimple.IsSimpleNum(571));
     }
 }
--- CheckNumberBySimple/CheckNumbySimple.cs
+++ CheckNumberBySimple/CheckNumbySimple.cs
@@ -1,17 +1,18 @@
 namespace CheckNumerBySimple
 {
-    public static class CheckNumbySimple
+    public static class CheckNumBySimple
     {
         public static bool IsSimpleNum(int num)
         {
-            for (int i = 2; i < num; i++)
-            {
-                if (num % i == 0)
-                {
-                    return false;
-                }
-            }
+            if (num <= 1) return false;
+            if (num <= 3) return true;
+            if (num % 2 == 0 || num % 3 == 0) return false;
 
+            for (int i = 5; i * i <= num; i += 6)
+            {
+                if (num % i == 0 || num % (i + 2) == 0)
+                    return false;
+            }
             return true;
         }
     }
